cd75032fb3471ae541c5ec868f5db24f631bbf17
leovieira84 <leonardo.vieira@gmail.com>
2024-12-29 20:27:46 -0300

    habilitar roteamento e definir rotas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component , inject} from '@angular/core';
+import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { CommonModule } from '@angular/common';
 import { HousingService } from '../housing.service';
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
+import {provideRouter} from '@angular/router';
+import routeConfig from './app/routes';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)]
+})
   .catch((err) => console.error(err));
