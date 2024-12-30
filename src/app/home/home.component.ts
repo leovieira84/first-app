3a8066a5dcb65ec377153821b8d9694827074036
leovieira84 <leonardo.vieira@gmail.com>
2024-12-30 00:12:20 -0300

    implementada a busca na página inicial

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { CommonModule } from '@angular/common';
 import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +12,25 @@ import { HousingService } from '../housing.service';
 })
 export class HomeComponent {
 
-  housingLocationList;
+  housingLocationList:HousingLocation[] = [];
+  filteredLocationList:HousingLocation[] = [];
 
   constructor(private housingService:HousingService){
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.housingService.getAllHousingLocations().then((housingLocationList:HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    })
+  }
+
+  filterResults(text:string){
+    console.log(text);
+    if(!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
